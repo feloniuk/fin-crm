2a5efeb8ec54e5415e6e908bb63834b76ef4f203
Serhii Feloniuk <s-feloniuk@ukr.net>
2026-01-31 22:24:18 +0200

    prod fixes

--- app/Filament/Resources/OurCompanyResource/Pages/ListOurCompanies.php
+++ app/Filament/Resources/OurCompanyResource/Pages/ListOurCompanies.php
@@ -1,8 +1,16 @@
 <?php
 namespace App\Filament\Resources\OurCompanyResource\Pages;
 use App\Filament\Resources\OurCompanyResource;
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 class ListOurCompanies extends ListRecords
 {
     protected static string $resource = OurCompanyResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
